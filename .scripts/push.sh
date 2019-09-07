37d1b966afc185b4d3cc5237c9899defcddfd5d4
1 <teyber@vps633045.ovh.net>
2019-09-07 23:19:57 +0200

    rm test files and pushing the new version of push.sh

--- .scripts/push.sh
+++ .scripts/push.sh
@@ -2,6 +2,11 @@
 
 inotifywait -m -r --event=modify,move,create --exclude /home/teyber/git/.git --format %w%f /home/teyber/git/ | 
 while read -r filename; do
+	git check-ignore $filename
+	if [ $? -eq 0 ]; then
+		echo "Ignored"
+		continue
+	fi
 	git add $filename
 	git commit -m "$filename"
 	git push origin master
